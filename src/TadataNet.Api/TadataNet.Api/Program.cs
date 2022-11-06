66e7eb8c7471e59ba281d8c150c2a7ed04f14b02
Karasek, Petr <caloris@caloris.cz>
2022-11-06 10:05:18 +0100

    basing configuration

--- src/TadataNet.Api/TadataNet.Api/Program.cs
+++ src/TadataNet.Api/TadataNet.Api/Program.cs
@@ -1,35 +1,58 @@
-namespace TadataNet.Api
+using Microsoft.AspNetCore.OData;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+using TadataNet.Common.Authorization;
+using TadataNet.Common.Helpers;
+
+namespace TadataNet.Api;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        var services = builder.Services;
+
+        services.AddDbContext<DataContext>();
+        services.AddCors();
+        services.AddControllers().AddJsonOptions(x =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            // serialize enums as strings in api responses (e.g. Role)
+            x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+        services.AddControllers().AddOData(options => options.Select().Filter().OrderBy());
+        services.AddHttpClient();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-            // Add services to the container.
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { 
+                Title = "TadataNet API", 
+                Version = "v1", 
+                Description = "Bookmark manager with many features"    
+            });
+        });
+        var app = builder.Build();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", ".NET Sign-up and Verification API"));
 
-            var app = builder.Build();
+        // global cors policy
+        app.UseCors(x => x
+            .SetIsOriginAllowed(origin => true)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+        // global error handler
+        app.UseMiddleware<ErrorHandlerMiddleware>();
 
-            app.UseHttpsRedirection();
+        // custom jwt auth middleware
+        app.UseMiddleware<JwtMiddleware>();
 
-            app.UseAuthorization();
+        app.MapControllers();
 
-
-            app.MapControllers();
-
-            app.Run();
-        }
+        app.Run();
     }
 }
